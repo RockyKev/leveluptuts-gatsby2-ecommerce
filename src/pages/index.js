0cc678f8ccb8f1d61f87748b616ab08d5e2d4d16
Rocky Kev <kev.rocky@gmail.com>
2020-01-04 15:26:59 -0800

    refactored product component

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,48 +3,23 @@ import { Link } from "gatsby";
 import CONFIG from "../../config";
 
 import Layout from "../components/layout";
-import Image from "../components/image";
+import Product from "../components/product";
 import SEO from "../components/seo";
 
 const IndexPage = () => {
-  const stripe = window.Stripe(CONFIG.stripe.testId);
-  const [sku, setSku] = useState("sku_GUKPMCuvZtxiRL");
-  // debugger
-
-  const placeOrder = () => {
-    stripe.redirectToCheckout({
-      //typically generated in local state
-      items: [
-        {
-          sku: sku,
-          quantity: 1,
-        },
-      ],
-      successUrl: "http://localhost:8000/success",
-      cancelUrl: "http://localhost:8000/cancel",
-    });
-  };
-
-  // console.log(window.Stripe)
-
   return (
     <Layout>
       <SEO title="Home" />
       <h1>Rocky Store</h1>
       <div>
-        <article>
-          <img src="https://picsum.photos/340/400" alt="Rocky T-shirt" />
-          <h3> Rocky T-shirt </h3>
-          <select defaultValue={sku} onChange={e => setSku(e.target.value)}>
-            <option value="sku_GUKPMCuvZtxiRL">Small</option>
-            <option value="sku_GUJfuYVpscOG5r">Medium</option>
-            <option value="sku_GUKPBzdhYoUGU3">Large</option>
-          </select>
-          <button onClick={() => placeOrder()}>
-            {/* <button onClick={() => placeOrder("sku_GUJfuYVpscOG5r")}> */}
-            Buy me
-          </button>
-        </article>
+        <Product />
+        <Product />
+
+        <Product />
+
+        <Product />
+
+        <Product />
       </div>
     </Layout>
   );
