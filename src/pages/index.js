54ae4bbe0dc8a86a0560d3d254a51e0fc8b6bf70
Rocky Kev <kev.rocky@gmail.com>
2020-01-04 15:00:26 -0800

    working buy button and hidden config

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,46 @@
 import React from "react"
 import { Link } from "gatsby"
+import CONFIG from "../../config"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-  </Layout>
-)
+const IndexPage = () => {
+  const stripe = window.Stripe(CONFIG.stripe.testId)
+  // debugger
+
+  const placeOrder = sku => {
+    stripe.redirectToCheckout({
+      //typically generated in local state
+      items: [
+        {
+          sku: sku,
+          quantity: 1,
+        },
+      ],
+      successUrl: "http://localhost:8000/",
+      cancelUrl: "http://localhost:8000/",
+    })
+  }
+
+  // console.log(window.Stripe)
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      <h1>Rocky Store</h1>
+      <div>
+        <article>
+          <img src="https://picsum.photos/340/400" alt="Rocky T-shirt" />
+          <h3> Rocky T-shirt </h3>
+          <button onClick={() => placeOrder("sku_GUJfuYVpscOG5r")}>
+            Buy me
+          </button>
+        </article>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
